8dd75d004ff5c8e4ebf5465572ed0568c53fb494
David <dk@schrittweiter.de>
2021-11-11 14:51:00 +0100

    added: columns field

---
+++ src/Fields/Columns.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Add columns field type to "WordPlate Extended"
+ *
+ * @link       https://schrittweiter.de
+ * @since      1.0.0
+ *
+ */
+
+/**
+ * Extend "WordPlate Extended" with columns by following plugin: Advanced Custom Fields: Extended PRO
+ *
+ * @link       https://www.acf-extended.com/features/fields/columns
+ * @version    0.8.8.6
+ *
+ * @author     ACF Extended <https://www.acf-extended.com/>
+ */
+
+namespace Schrittweiter\Acf\Fields;
+
+use WordPlate\Acf\Fields\Attributes\ConditionalLogic;
+use WordPlate\Acf\Fields\Field;
+
+class Columns extends Field
+{
+
+	use ConditionalLogic;
+
+	protected $type = 'acfe_column';
+
+	/**
+	 * set defaults on call
+	 *
+	 * @param string $label
+	 * @param string|null $name
+	 */
+	public function __construct(string $label, ?string $name = null)
+	{
+		parent::__construct($label, $name);
+	}
+
+	/**
+	 * Columns
+	 * Options: auto, fill, 1/12, 2/12, 3/12, 4/12, 5/12, 6/12, 7/12, 8/12, 9/12, 10/12, 11/12, 12/12
+	 *
+	 * @param string $value
+	 *
+	 * @return $this
+	 */
+	public function columns(string $value): self
+	{
+		$this->config->set('columns', $value);
+
+		return $this;
+	}
+
+	/**
+	 * endpoint
+	 * Define an endpoint for the previous columns to stop.
+	 *
+	 * @return $this
+	 */
+	public function endpoint(): self
+	{
+		$this->config->set('endpoint', true);
+
+		return $this;
+	}
+
+	/**
+	 * Border
+	 * Enable “Column border” and “Fields border”
+	 *
+	 * Options: column, fields
+	 *
+	 * @return $this
+	 */
+	public function border(array $border): self
+	{
+		$this->config->set('border', $border);
+
+		return $this;
+	}
+}
